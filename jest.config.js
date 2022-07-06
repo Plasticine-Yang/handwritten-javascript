c3871b8e3c1325f077a69ea286df3fbfbc0b17e1
plasticine9750 <975036719@qq.com>
2022-07-06 08:23:34 +0800

    chore(jest): make fakeTimers enable globally

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,10 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js', 'json'],
   rootDir: __dirname,
   testMatch: ['<rootDir>/src/**/__test__/**/*.spec.[jt]s?(x)'],
-  testPathIgnorePatterns: ['/node_modules/']
+  testPathIgnorePatterns: ['/node_modules/'],
+  // 让 setTimeout、setInterval 等计时器 API 变成 fakeTimer
+  // 否则就需要在每个用到计时器的单元测试中手动调用一下 jest.useFakeTimers
+  fakeTimers: {
+    enableGlobally: true
+  }
 }
