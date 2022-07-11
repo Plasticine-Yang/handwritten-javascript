418faa195d2488edf51241c54f85e7420f9b6905
plasticine9750 <975036719@qq.com>
2022-07-11 22:43:19 +0800

    chore(event-emitter): build and test config

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   rootDir: __dirname,
-  testMatch: ['<rootDir>/src/**/__test__/**/*.spec.[jt]s?(x)'],
+  testMatch: ['<rootDir>/packages/**/__test__/**/*.spec.[jt]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/'],
   // 让 setTimeout、setInterval 等计时器 API 变成 fakeTimer
   // 否则就需要在每个用到计时器的单元测试中手动调用一下 jest.useFakeTimers
