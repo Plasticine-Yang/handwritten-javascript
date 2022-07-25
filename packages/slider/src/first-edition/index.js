3318db553e67f30a0f14e0308468483dba9e0903
plasticine <975036719@qq.com>
2022-07-25 17:01:29 +0800

    feat(slider): first-edition

--- packages/slider/src/first-edition/index.js
+++ packages/slider/src/first-edition/index.js
@@ -4,6 +4,29 @@ class Slider {
     this.items = this.container.querySelectorAll(
       '.slider__item, .slider__item--selected'
     )
+
+    const controller = this.container.querySelector('.slider__control')
+    if (controller) {
+      // 获取到所有的控制条项按钮
+      const buttons = controller.querySelectorAll(
+        '.slider__control-buttons, .slider__control-buttons--selected'
+      )
+      // 添加对 `slide` 事件的监听，修改控制条的状态
+      this.container.addEventListener('slide', e => {
+        // 从事件携带数据中获取到下一个轮播图下标
+        const idx = e.detail.index
+        // 获取到当前选中的控制条项
+        const selected = controller.querySelector(
+          '.slider__control-buttons--selected'
+        )
+        if (selected) {
+          // 移除旧的选中项
+          selected.className = 'slider__control-buttons'
+        }
+        // 给新的控制条项添加选中状态
+        buttons[idx].className = 'slider__control-buttons--selected'
+      })
+    }
   }
 
   getSelectedItem() {
@@ -27,6 +50,12 @@ class Slider {
     if (targetItem) {
       targetItem.className = 'slider__item--selected'
     }
+
+    // 使用自定义事件广播一个 `slide` 事件出去 通知相关状态发生变化
+    // 广播事件时携带的数据
+    const detail = { index: idx }
+    const event = new CustomEvent('slide', { bubbles: true, detail })
+    this.container.dispatchEvent(event)
   }
 
   slideNext() {
